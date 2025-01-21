d3a8522d10d616dd076f2617aee2ab68a4c2d355
Constantin Müller <info@mueller-constantin.de>
2025-01-21 22:08:58 +0100

    feat: add stack template for pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,9 @@
-import Navbar from "@/components/organisms/Navbar";
-import Footer from "@/components/molecules/Footer";
+import StackTemplate from "@/components/templates/StackTemplate";
 
 export default function Home() {
   return (
-    <div className="h-full min-h-screen flex flex-col">
-      <header>
-        <Navbar currentPath="/" />
-      </header>
-      <main className="grow flex flex-col text-gray-800">
-
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+    <StackTemplate>
+      
+    </StackTemplate>
   );
 }
---
+++ src/components/templates/StackTemplate.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Navbar from "@/components/organisms/Navbar";
+import Footer from "@/components/molecules/Footer";
+
+export default function StackTemplate({ children }) {
+  return (
+    <div className="h-full min-h-screen flex flex-col">
+      <header>
+        <Navbar currentPath="/" />
+      </header>
+      <main className="grow flex flex-col">
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
