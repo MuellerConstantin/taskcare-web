fe277c2e067749c8eb0212a5b78e340d291df44a
Constantin Müller <info@mueller-constantin.de>
2025-02-13 19:22:00 +0100

    feat: add minimal board's backlog page

---
+++ src/app/boards/[boardId]/backlog/layout.jsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "TaskCare | Board - Backlog"
+};
+
+export default function BoardBacklogLayout({ children }) {
+  return children;
+}
---
+++ src/app/boards/[boardId]/backlog/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function BoardBacklog() {
+  return (
+    <div className="flex flex-col grow w-full max-w-screen-2xl mx-auto p-4 space-y-4" />
+  );
+}
