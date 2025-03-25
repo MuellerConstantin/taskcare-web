2569268a5f1a662839b0effcafc0232ebd085131
Constantin Müller <info@mueller-constantin.de>
2025-03-25 17:34:17 +0100

    feat: add minimal pagination component

---
+++ src/components/molecules/Pagination.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { Pagination } from "./Pagination";
+
+const meta: Meta<typeof Pagination> = {
+  title: "Molecules/Pagination",
+  component: Pagination,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Pagination> = {
+  args: {
+    totalPages: 10,
+    currentPage: 1,
+  },
+};
+
+export const Small: StoryObj<typeof Pagination> = {
+  args: {
+    totalPages: 10,
+    currentPage: 10,
+    size: "sm",
+  },
+};
+
+export const Medium: StoryObj<typeof Pagination> = {
+  args: {
+    totalPages: 10,
+    currentPage: 3,
+    size: "md",
+  },
+};
+
+export const Large: StoryObj<typeof Pagination> = {
+  args: {
+    totalPages: 10,
+    currentPage: 7,
+    size: "lg",
+  },
+};
